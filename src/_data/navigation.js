dccbd57b78bb20837c8fbfcd9103f7356702ad1d
Alec Campbell <88142969+22940dev@users.noreply.github.com>
2021-08-11 15:04:04 -0400

    Update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -3,13 +3,11 @@ module.exports = {
     { name: 'Blog', link: '/blog/' },
     { name: 'Projects', link: '/projects/' },
     { name: 'Bookmarks', link: '/bookmarks/' },
-    { name: 'Contact', link: '/contact/' },
-    { name: 'test', link: '/test/' }
+    { name: 'Contact', link: '/contact/' }
   ],
   external: [
     { icon: 'github', link: 'https://github.com/fu4303' },
     { icon: 'linkedin', link: 'https://www.linkedin.com/in/doc22940/' },
-    { icon: 'dev', link: 'https://dev.to/doc22940' },
-    { icon: 'codepen', link: 'https://codepen.io/doc22940' },
+    { icon: 'dev', link: 'https://dev.to/doc22940' }
   ],
 }
